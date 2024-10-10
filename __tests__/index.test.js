5ce30e1c011c8a60b354bb77157a2e7b4a601e39
ysromantic <ysromantic@gmail.com>
2024-10-10 13:14:31 +0300

    Add reverse function test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,14 @@
 import reverse from '../src/index.js';
+import fs from 'fs';
+import path from 'path';
 
-test('reverse', () => {
-    expect(reverse('hello')).toEqual('olleh');
-    expect(reverse('')).toEqual('');
+test('reverse text from file', () => {
+    const dataPath = path.join(__dirname, '..', '__fixtures__', 'Data.txt');
+    const resultPath = path.join(__dirname, '..', '__fixtures__', 'result.txt');
+
+    const text = fs.readFileSync(dataPath, 'utf-8');
+    const reversedText = reverse(text);
+    const expectedReversedText = fs.readFileSync(resultPath, 'utf-8');
+
+    expect(reversedText).toEqual(expectedReversedText);
 });
